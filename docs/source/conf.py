26330618ce086155e82331519ad47f770839bd23
Konstantin Löffler <kl@wip.tu-berlin.de>
2025-01-10 13:18:12 +0100

    more style testing

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,7 +2,7 @@
 
 # -- Project information
 
-project = 'GENeSYS-MOD'
+project = 'GENeSYS-MOD | The Global Energy System Model'
 copyright = '2024, The GENeSYS-MOD Community'
 author = 'The GENeSYS-MOD Community'
 
@@ -38,8 +38,8 @@ html_css_files = ["custom.css"]
 html_theme_options = {
     #'analytics_id': 'G-XXXXXXXXXX',  #  Provided by Google in your dashboard
     #'analytics_anonymize_ip': False,
-    'logo_only': False,
-    'prev_next_buttons_location': 'bottom',
+    'logo_only': True,
+    'prev_next_buttons_location': 'None',
     'style_external_links': False,
     #'vcs_pageview_mode': '',
     #'style_nav_header_background': 'white',
